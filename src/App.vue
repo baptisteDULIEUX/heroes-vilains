6fe1ad63238b99d5c8413ea048fa007e481305ec
agaiffe2-iut90 <anna.gaiffe@edu.univ-fcomte.fr>
2025-04-06 16:12:57 +0200

    ajout de l'onglet héro

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,51 @@
 <template>
-  <v-app>
-    <v-navigation-drawer app v-model="drawer">
-      <v-list>
-        <v-list-item-group>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <router-link to="/">Home</router-link>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <router-link to="/auth">Auth</router-link>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-domain</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <router-link to="/organizations">Organizations</router-link>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account-group</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <router-link to="/teams">Teams</router-link>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-
+    <v-app>
+        <v-navigation-drawer app v-model="drawer">
+            <v-list>
+                <v-list-item-group>
+                    <v-list-item>
+                        <v-list-item-icon>
+                            <v-icon>mdi-home</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <router-link to="/">Home</router-link>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-list-item-icon>
+                            <v-icon>mdi-account</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <router-link to="/auth">Auth</router-link>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-list-item-icon>
+                            <v-icon>mdi-domain</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <router-link to="/organizations">Organizations</router-link>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-list-item-icon>
+                            <v-icon>mdi-account-group</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <router-link to="/teams">Teams</router-link>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-list-item-icon>
+                            <v-icon>mdi-flash</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <router-link to="/heroes">Héros</router-link>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>My App</v-toolbar-title>
